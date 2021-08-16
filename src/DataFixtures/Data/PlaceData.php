105ac229754f113bb59ffa78b90ad072e5d1d4f9
Teddy Dahyot <teddy.dahyot@hotmail.fr>
2021-08-16 14:48:07 +0200

    Update PlaceData.php

--- src/DataFixtures/Data/PlaceData.php
+++ src/DataFixtures/Data/PlaceData.php
@@ -23,22 +23,49 @@ class PlaceData
             'picture' => 'https://fr.wikipedia.org/wiki/Mus%C3%A9e_des_Beaux-Arts_de_Dijon#/media/Fichier:Entr%C3%A9e_du_mus%C3%A9e_des_Beaux-Arts.jpg'
         ],
 
-        // [
-        //     'name' => 'Hôtel de Vogüé',
-        //     'address' => '8 Rue de la Chouette',
-        //     'latitude' => 47.33122550292202,
-        //     'longitude' => 5.043896453201671,
-        //     'description' => 'L\'Hôtel de Vogüé de Dijon en Côte-d\'Or est un hôtel particulier du xviie siècle. Il est situé 8 rue de la Chouette, près du chevet de l\'église Notre-Dame. L\'hôtel a été classé au titre des monuments historiques le 5 janvier 1911',
-        //     'picture' => 'https://fr.wikipedia.org/wiki/H%C3%B4tel_de_Vog%C3%BC%C3%A9#/media/Fichier:Vue_panoramique_de_Dijon_10.jpg'
-        // ],
+        [
+            'name' => 'Cour Bareuzai',
+            'address' => '61 Rue des Godrans',
+            'latitude' => 47.32253972478744,
+            'longitude' => 5.038648910961855,
+            'description' => 'Cour Bareuzai se situe à Dijon, Capitale de la région Bourgogne Franche Comté, au sein du centre-ville historique classé depuis juillet 2015 au patrimoine mondial de l’Unesco. Passage atypique, c’est à la fois un lieu unique et central permettant d’offrir une expérience nouvelle grâce à un projet de rénovation ambitieux.',
+            'picture' => 'https://chroniques-architecture.com/wp-content/uploads/2021/04/01-Cour-Bareuzai-Chapman-Taylor-@Stephane-Rouillard.jpg'
+        ],
 
-        // [
-        //     'name' => 'Hôtel de Vogüé',
-        //     'address' => '8 Rue de la Chouette',
-        //     'latitude' => 47.33122550292202,
-        //     'longitude' => 5.043896453201671,
-        //     'description' => 'L\'Hôtel de Vogüé de Dijon en Côte-d\'Or est un hôtel particulier du xviie siècle. Il est situé 8 rue de la Chouette, près du chevet de l\'église Notre-Dame. L\'hôtel a été classé au titre des monuments historiques le 5 janvier 1911',
-        //     'picture' => 'https://fr.wikipedia.org/wiki/H%C3%B4tel_de_Vog%C3%BC%C3%A9#/media/Fichier:Vue_panoramique_de_Dijon_10.jpg'
-        // ],
+        [
+            'name' => '',
+            'address' => '',
+            'latitude' => ,
+            'longitude' => ,
+            'description' => '',
+            'picture' => ''
+        ],
+
+        [
+            'name' => '',
+            'address' => '',
+            'latitude' => ,
+            'longitude' => ,
+            'description' => '',
+            'picture' => ''
+        ],
+
+        [
+            'name' => '',
+            'address' => '',
+            'latitude' => ,
+            'longitude' => ,
+            'description' => '',
+            'picture' => ''
+        ],
+
+        [
+            'name' => '',
+            'address' => '',
+            'latitude' => ,
+            'longitude' => ,
+            'description' => '',
+            'picture' => ''
+        ],
     ];
 }
