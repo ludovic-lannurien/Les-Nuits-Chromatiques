59be88c97ca30f24a8729d573cd6ce3158cdadad
Teddy Dahyot <teddy.dahyot@hotmail.fr>
2021-08-16 14:28:38 +0200

    Create PlaceFixtures + PlaceData

---
+++ src/DataFixtures/Data/PlaceData.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\DataFixtures\Data;
+
+class PlaceData
+{
+    public static $placeData = [
+        [
+            'name' => 'Hôtel de Vogüé',
+            'address' => '8 Rue de la Chouette',
+            'latitude' => 47.33122550292202,
+            'longitude' => 5.043896453201671,
+            'description' => 'L\'Hôtel de Vogüé de Dijon en Côte-d\'Or est un hôtel particulier du xviie siècle. Il est situé 8 rue de la Chouette, près du chevet de l\'église Notre-Dame. L\'hôtel a été classé au titre des monuments historiques le 5 janvier 1911',
+            'picture' => 'https://fr.wikipedia.org/wiki/H%C3%B4tel_de_Vog%C3%BC%C3%A9#/media/Fichier:Vue_panoramique_de_Dijon_10.jpg'
+        ],
+    ];
+}
--- src/DataFixtures/PlaceFixtures.php
+++ src/DataFixtures/PlaceFixtures.php
@@ -2,15 +2,49 @@
 
 namespace App\DataFixtures;
 
+use App\DataFixtures\Data\PlaceData;
+use App\Entity\Place;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\DBAL\Connection;
 use Doctrine\Persistence\ObjectManager;
 
-class AppFixtures extends Fixture
+class PlaceFixtures extends Fixture
 {
+    private $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    private function truncate()
+    {
+        $this->connection->executeQuery('SET foreign_key_checks = 0');
+        $this->connection->executeQuery('TRUNCATE TABLE artist');
+        $this->connection->executeQuery('TRUNCATE TABLE event');
+        $this->connection->executeQuery('TRUNCATE TABLE place');
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->truncate();
+
+        foreach (PlaceData::$placeData as $data) {
+            
+            $place = new Place();
+            
+            $place->setName($data['name']);
+            $place->setAddress($data['address']);
+            $place->setZipCode(21000);
+            $place->setCity('Dijon');
+            $place->setLatitude($data['latitude']);
+            $place->setLongitude($data['longitude']);
+            $place->setDescription($data['description']);
+            $place->setPicture($data['picture']);
+            $place->setSlug('test');
+
+            $manager->persist($place);
+        };
 
         $manager->flush();
     }
