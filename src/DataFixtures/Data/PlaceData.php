c8f57c81aa6e373c80a7bc090153a74398562e15
Teddy Dahyot <teddy.dahyot@hotmail.fr>
2021-08-16 15:24:14 +0200

    Update PlaceFixtures.php

--- src/DataFixtures/Data/PlaceData.php
+++ src/DataFixtures/Data/PlaceData.php
@@ -33,39 +33,39 @@ class PlaceData
         ],
 
         [
-            'name' => '',
-            'address' => '',
-            'latitude' => ,
-            'longitude' => ,
-            'description' => '',
-            'picture' => ''
+            'name' => 'La Vapeur',
+            'address' => '42 Avenue de Stalingrad',
+            'latitude' => 47.40590302174257,
+            'longitude' => 5.060213182853193,
+            'description' => 'Site officiel de La Vapeur, scène de musiques actuelles de la ville de Dijon. Concerts, expositions, projections, actions culturelles et artistiques.',
+            'picture' => 'https://www.souchier-boullet.com/wp-content/uploads/2019/01/lavapeur-dijon.jpg'
         ],
 
         [
-            'name' => '',
-            'address' => '',
-            'latitude' => ,
-            'longitude' => ,
-            'description' => '',
-            'picture' => ''
+            'name' => 'Galeries Lafayette',
+            'address' => '41-49 Rue de la Liberté',
+            'latitude' => 47.43766312827879,
+            'longitude' => 5.014112175752159,
+            'description' => 'Galeries du Groupe Galeries Lafayette. Le bâtiment se situe à l\'angle de la rue du château et de la rue de la Liberté.',
+            'picture' => 'https://cdn-s-www.bienpublic.com/images/3B61ACB0-8B7E-4628-B529-E8F95B0CA2B8/NW_raw/1-quatre-vingt-dix-ans-apres-la-facade-des-galeries-lafayette-a-garde-toute-sa-prestance-en-particulier-en-plein-centre-ville-de-dijon-2-aujourd-hui-le-magasin-bourguignon-propose-plusieurs-centaines-de-marques-photos-p-b-1411740300.jpg'
         ],
 
         [
-            'name' => '',
-            'address' => '',
-            'latitude' => ,
-            'longitude' => ,
-            'description' => '',
-            'picture' => ''
+            'name' => 'Cour de l\'ENSA',
+            'address' => '3 Rue Michelet',
+            'latitude' => 47.45752729473983,
+            'longitude' => 5.014112017943309,
+            'description' => 'La Cour de l\'ENSA est la cour de L\'École nationale supérieure d\'art de Dijon, anciennement l\'École nationale des beaux-arts de Dijon.',
+            'picture' => 'https://ensa-dijon.fr/wp-content/uploads/2020/10/cite-internationale-arts-paris.jpg'
         ],
 
         [
-            'name' => '',
-            'address' => '',
-            'latitude' => ,
-            'longitude' => ,
-            'description' => '',
-            'picture' => ''
-        ],
+            'name' => 'Grand Hôtel La Cloche',
+            'address' => '14 Place Darcy',
+            'latitude' => 47.32432869558158,
+            'longitude' => 5.034022511586989,
+            'description' => 'Grand Hôtel La Cloche de Dijon est un hôtel 5 étoiles classé monument historique qui propose des chambres et suites en plein centre ville.',
+            'picture' => 'https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_1300,q_auto,w_2000/itemimages/48/69/48691_v4.jpeg'
+        ]
     ];
 }
--- src/Entity/Place.php
+++ src/Entity/Place.php
@@ -55,7 +55,7 @@ class Place
     private $description;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=320)
      */
     private $picture;
 
