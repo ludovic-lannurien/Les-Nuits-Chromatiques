6fc1117199cc1400f79d2f1fa3263d0c70db1a5b
guillaume806 <guillaumebodart@outlook.fr>
2021-08-16 15:21:52 +0200

    fixtures-artist

---
+++ src/DataFixtures/Data/ArtistData.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\DataFixtures\Data;
+
+class ArtistData
+{  
+   public static $artistData = 
+   [
+
+
+        [
+            'firstname'=>'WECOMEINPEACE',
+            'lastname'=>'',
+            'description'=>'WECOMEINPEACE a été fondé par Wilfried Della Rossa, Kimi Do et Thomas Mathieu Depuis 2008, 
+            ils produisent des évènements et des installations autour de concepts innovants et créatifs.
+            Ils puisent leur inspiration dans différents domaines tels que le design, le graphisme, l’architecture et le jeu vidéo.',
+            'picture'=>'https://www.flickr.com/photos/_wecomeinpeace/16076592071/',
+            'type'=>'lumiére',
+            'videolink'=>'https://www.youtube.com/watch?v=1xJQ816CTKQ&ab_channel=CHKT',
+        ],
+
+        [
+            'firstname'=>'THEORIZ',
+            'lastname'=>'',
+            'description'=>'Théoriz est un studio de création Art et Technologie spécialisé dan sla conception installations immersives et de spectacles audiovisuels innovant',
+            'picture'=>'',
+            'type'=>'lumiére',
+            'videolink'=>'https://vimeo.com/194838088',
+        ],
+
+        [
+            'firstname'=>'SAPIA',
+            'lastname'=>'',
+            'description'=>'',
+            'picture'=>'',
+            'type'=>'musique',
+            'videolink'=>'https://soundcloud.com/adrienpisa/bebe-requin-dj-set',
+        ],
+
+        [
+            'firstname'=>'GUERRA',
+            'lastname'=>'david',
+            'description'=> 'Svindrön, aka David Guerra, est un artiste sonore, compositeur, interprète, sound designer et programmeur, vivant en France.
+            Multi-instrumentiste autodidacte et créateur de musique électronique, il se forme à la musique électroacoustique (Conservatoire 
+            de Chalon-sur-Saône, avec Jean-Marc WEBER) et aux techniques studio (CFPM Villeurbanne).
+            C’est au travers d’un live électro IDM / techno qu’il fait ses armes sur la scène lyonnaise. Aujourd’hui, par ses pratiques
+            et son parcours, il lie à sa production musicale la précision d’une approche électroacoustique, l’efficacité des rythmes techno, 
+            la puissance des nappes de la musique drone et la chaleur boisée d’une guitare, ou, d’une sanza.
+            Sensible à l’art total, David Guerra crée et collabore sur de nombreux projets artistiques où l’art sonore, la danse, l’art visuel, 
+            l’art plastique... vivent dans un temps unique.',
+            'picture'=>'',
+            'type'=>'musique/lumiere',
+            'videolink'=>'https://vimeo.com/161602302',
+        ],
+    ];
+
+
+}
