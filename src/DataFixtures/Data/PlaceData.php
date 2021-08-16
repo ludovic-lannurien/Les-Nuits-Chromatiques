1109532eeb77689b428a211af8c07f7736bd47af
Teddy Dahyot <teddy.dahyot@hotmail.fr>
2021-08-16 14:39:46 +0200

    Update PlaceData.php

--- src/DataFixtures/Data/PlaceData.php
+++ src/DataFixtures/Data/PlaceData.php
@@ -13,5 +13,32 @@ class PlaceData
             'description' => 'L\'Hôtel de Vogüé de Dijon en Côte-d\'Or est un hôtel particulier du xviie siècle. Il est situé 8 rue de la Chouette, près du chevet de l\'église Notre-Dame. L\'hôtel a été classé au titre des monuments historiques le 5 janvier 1911',
             'picture' => 'https://fr.wikipedia.org/wiki/H%C3%B4tel_de_Vog%C3%BC%C3%A9#/media/Fichier:Vue_panoramique_de_Dijon_10.jpg'
         ],
+
+        [
+            'name' => 'Musée des Beaux-Arts de Dijon',
+            'address' => '1 Rue Rameau',
+            'latitude' => 47.34670216857804,
+            'longitude' => 5.0303548436853385,
+            'description' => 'Le musée des Beaux-Arts de Dijon est l\'un des plus importants et l\'un des plus anciens musées de France. Situé au cœur de la ville, il occupe l\'ancien palais ducal, siège au XVᵉ siècle de l’État bourguignon.',
+            'picture' => 'https://fr.wikipedia.org/wiki/Mus%C3%A9e_des_Beaux-Arts_de_Dijon#/media/Fichier:Entr%C3%A9e_du_mus%C3%A9e_des_Beaux-Arts.jpg'
+        ],
+
+        // [
+        //     'name' => 'Hôtel de Vogüé',
+        //     'address' => '8 Rue de la Chouette',
+        //     'latitude' => 47.33122550292202,
+        //     'longitude' => 5.043896453201671,
+        //     'description' => 'L\'Hôtel de Vogüé de Dijon en Côte-d\'Or est un hôtel particulier du xviie siècle. Il est situé 8 rue de la Chouette, près du chevet de l\'église Notre-Dame. L\'hôtel a été classé au titre des monuments historiques le 5 janvier 1911',
+        //     'picture' => 'https://fr.wikipedia.org/wiki/H%C3%B4tel_de_Vog%C3%BC%C3%A9#/media/Fichier:Vue_panoramique_de_Dijon_10.jpg'
+        // ],
+
+        // [
+        //     'name' => 'Hôtel de Vogüé',
+        //     'address' => '8 Rue de la Chouette',
+        //     'latitude' => 47.33122550292202,
+        //     'longitude' => 5.043896453201671,
+        //     'description' => 'L\'Hôtel de Vogüé de Dijon en Côte-d\'Or est un hôtel particulier du xviie siècle. Il est situé 8 rue de la Chouette, près du chevet de l\'église Notre-Dame. L\'hôtel a été classé au titre des monuments historiques le 5 janvier 1911',
+        //     'picture' => 'https://fr.wikipedia.org/wiki/H%C3%B4tel_de_Vog%C3%BC%C3%A9#/media/Fichier:Vue_panoramique_de_Dijon_10.jpg'
+        // ],
     ];
 }
