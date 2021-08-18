b4fe53642b2dcc95cd9404031ecc3366b4b0785b
Teddy DAHYOT <61130381+teddydah@users.noreply.github.com>
2021-08-18 15:49:27 +0200

    Update EventData.php

--- src/DataFixtures/Data/EventData.php
+++ src/DataFixtures/Data/EventData.php
@@ -12,32 +12,32 @@ class EventData
             'end_datetime' => '07-03-2022 23:00:00'
         ],
 
-        // [
-        //     'name' => 'Apéro Lumière',
-        //     'description' => 'L\'équipe CHKT, vous propose un apéro mix qui aura pour objectif de vous faire oublier votre journée de travail',
-        //     'start_datetime' => '8 Mars 2022 - 16h30',
-        //     'end_datetime' => '8 Mars 2022 - 18h30',
-        // ],
+        [
+            'name' => 'Apéro Lumière',
+            'description' => 'L\'équipe CHKT, vous propose un apéro mix qui aura pour objectif de vous faire oublier votre journée de travail',
+            'start_datetime' => '07-03-2022 19:30:00',
+            'end_datetime' => '07-03-2022 23:00:00',
+        ],
 
-        // [
-        //     'name' => 'Lumière sur cour',
-        //     'description' => '',
-        //     'start_datetime' => '8 Mars 2022 - 19h30',
-        //     'end_datetime' => '',
-        // ],
+        [
+            'name' => 'Lumière sur cour',
+            'description' => 'test',
+            'start_datetime' => '07-03-2022 19:30:00',
+            'end_datetime' => '07-03-2022 23:00:00',
+        ],
 
-        // [
-        //     'name' => 'Patrimoine illuminé',
-        //     'description' => '',
-        //     'start_datetime' => '9 Mars 2022 - 19h30',
-        //     'end_datetime' => '',
-        // ],
+        [
+            'name' => 'Patrimoine illuminé',
+            'description' => 'test',
+            'start_datetime' => '07-03-2022 19:30:00',
+            'end_datetime' => '07-03-2022 23:00:00',
+        ],
 
-        // [
-        //     'name' => 'La forêt de lumière',
-        //     'description' => '',
-        //     'start_datetime' => '10 Mars 2022 - 19h30',
-        //     'end_datetime' => '',
-        // ],
+        [
+            'name' => 'La forêt de lumière',
+            'description' => 'test',
+            'start_datetime' => '07-03-2022 19:30:00',
+            'end_datetime' => '07-03-2022 23:00:00',
+        ],
     ];
 }
