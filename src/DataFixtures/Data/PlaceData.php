f05abbc1a2f3e5aafb525c656d993f3bb983bea4
Teddy Dahyot <teddy.dahyot@hotmail.fr>
2021-08-19 16:59:26 +0200

    Update PlaceData.php

--- src/DataFixtures/Data/PlaceData.php
+++ src/DataFixtures/Data/PlaceData.php
@@ -11,7 +11,7 @@ class PlaceData
             'latitude' => 47.33122550292202,
             'longitude' => 5.043896453201671,
             'description' => 'L\'Hôtel de Vogüé de Dijon en Côte-d\'Or est un hôtel particulier du xviie siècle. Il est situé 8 rue de la Chouette, près du chevet de l\'église Notre-Dame. L\'hôtel a été classé au titre des monuments historiques le 5 janvier 1911',
-            'picture' => 'https://fr.wikipedia.org/wiki/H%C3%B4tel_de_Vog%C3%BC%C3%A9#/media/Fichier:Vue_panoramique_de_Dijon_10.jpg'
+            'picture' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Vue_panoramique_de_Dijon_10.jpg/1024px-Vue_panoramique_de_Dijon_10.jpg'
         ],
 
         [
@@ -20,7 +20,7 @@ class PlaceData
             'latitude' => 47.34670216857804,
             'longitude' => 5.0303548436853385,
             'description' => 'Le musée des Beaux-Arts de Dijon est l\'un des plus importants et l\'un des plus anciens musées de France. Situé au cœur de la ville, il occupe l\'ancien palais ducal, siège au XVᵉ siècle de l’État bourguignon.',
-            'picture' => 'https://fr.wikipedia.org/wiki/Mus%C3%A9e_des_Beaux-Arts_de_Dijon#/media/Fichier:Entr%C3%A9e_du_mus%C3%A9e_des_Beaux-Arts.jpg'
+            'picture' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Entr%C3%A9e_du_mus%C3%A9e_des_Beaux-Arts.jpg/1024px-Entr%C3%A9e_du_mus%C3%A9e_des_Beaux-Arts.jpg'
         ],
 
         [
