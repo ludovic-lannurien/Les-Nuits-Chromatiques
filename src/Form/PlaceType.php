eadf06cfe79a10e9dff4eb03800c30e9f07a4a4c
Teddy Dahyot <teddy.dahyot@hotmail.fr>
2021-08-19 15:02:55 +0200

    Create PlaceController

---
+++ src/Form/PlaceType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PlaceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('address')
+            ->add('zipCode')
+            ->add('city')
+            ->add('longitude')
+            ->add('latitude')
+            ->add('description')
+            ->add('picture')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
