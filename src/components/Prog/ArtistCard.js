8e86815182dae736d9126e2248058a505f49c6e7
Kilian-mescam <kmescam@gmail.com>
2021-08-20 14:03:22 +0200

    prog with scroll ok

--- src/components/Prog/ArtistCard.js
+++ src/components/Prog/ArtistCard.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import philippe from './katerine.jpg';
 
 const ArtistCard = ({ firstname }) => {
   console.log(firstname);
   return (
     <div className="artist">
-      <a><h1>{firstname}</h1></a>
+      <a href="#"><h1>{firstname}</h1></a>
+      <a href="#"><img src={philippe} alt={firstname} /></a>
     </div>
   );
 };
