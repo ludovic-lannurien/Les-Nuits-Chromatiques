77309f36969a99227d71c4cb78e63330daf62445
Kilian-mescam <kmescam@gmail.com>
2021-08-20 14:28:10 +0200

    i changed location of dayfilter to put it on map's component. now i doisn't appear when wi click on programmation

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -8,7 +8,6 @@ import Nav from 'src/components/Nav';
 import Prog from 'src/components/Prog';
 import Festival from 'src/components/Festival';
 import Places from 'src/components/Places';
-import DayFilter from './DayFilter';
 import './app.scss';
 
 // == Composant
@@ -16,7 +15,6 @@ const App = () => (
   <div className="app">
     <Nav />
     <div className="wrapper">
-      <DayFilter />
       <Route path="/" exact>
         <Map />
       </Route>
