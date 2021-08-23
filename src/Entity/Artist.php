14205eb79fbcdfc8b5e9a254b286b09516224d1e
Teddy DAHYOT <61130381+teddydah@users.noreply.github.com>
2021-08-23 22:49:53 +0200

    Update templates + entities

--- src/Entity/Artist.php
+++ src/Entity/Artist.php
@@ -6,11 +6,13 @@ use App\Repository\ArtistRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=ArtistRepository::class)
+ * @UniqueEntity(fields={"firstname"})
  */
 class Artist
 {
@@ -65,7 +67,7 @@ class Artist
     private $description;
 
     /**
-     * @ORM\Column(type="text", nullable=true)
+     * @ORM\Column(type="text")
      * @Groups("artists_get")
      * @Groups("events_get")
      * @Groups("genres_get")
