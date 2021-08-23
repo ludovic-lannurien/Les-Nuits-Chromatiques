e0ff34744dd96341f20b82409e5ba209230be997
Kilian-mescam <kmescam@gmail.com>
2021-08-23 10:26:16 +0200

    integration artist page

--- src/components/Prog/Artist/index.js
+++ src/components/Prog/Artist/index.js
@@ -32,7 +32,7 @@ const Artist = () => (
       </div>
 
       <div className="video">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/yObFudZy59s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/6JMCgVFYAqQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" className="video-iframe" allowfullscreen></iframe>
       </div>
     </div>
   </div>
