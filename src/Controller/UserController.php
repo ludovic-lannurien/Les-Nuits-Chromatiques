c3226bff22602e9a90324f11d20ef78497150a5d
Teddy DAHYOT <61130381+teddydah@users.noreply.github.com>
2021-08-23 13:49:12 +0200

    Create UserController

---
+++ src/Controller/UserController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Form\UserEditType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/admin/user/browse", name="admin_user_browse", methods={"GET"})
+     */
+    public function browse(UserRepository $userRepository): Response
+    {
+        return $this->render('back/user/browse.html.twig', [
+            'users' => $userRepository->findAll()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/user/read/{id}", name="admin_user_read", methods={"GET"})
+     */
+    public function read(User $user = null): Response
+    {
+        // 404 ?
+        if (null === $user) {
+            throw $this->createNotFoundException('Utilisateur non trouvé.');
+        }
+
+        return $this->render('back/user/read.html.twig', [
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * @Route("/admin/user/edit/{id}", name="admin_user_edit", methods={"GET", "POST"})
+     */
+    public function edit(User $user = null, Request $request, UserPasswordHasherInterface $userPasswordHasher): Response
+    {
+        // 404 ?
+        if (null === $user) {
+            throw $this->createNotFoundException('Utilisateur non trouvé.');
+        }
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            if (!empty($form->get('password')->getData())) {
+
+                $hashedPassword = $userPasswordHasher->hashPassword($user, $form->get('password')->getData());
+
+                $user->setPassword($hashedPassword);
+            }
+
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('admin_user_browse', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form
+        ]);
+    }
+
+    /**
+     * @Route("/admin/user/add", name="admin_user_add", methods={"GET", "POST"})
+     */
+    public function add(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface): Response
+    {
+        $user = new User();
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $hashedPassword = $userPasswordHasherInterface->hashPassword($user, $user->getPassword());
+
+            $user->setPassword($hashedPassword);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('back_user_browse', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('user/add.html.twig', [
+            'user' => $user,
+            'form' => $form
+        ]);
+    }
+
+    /**
+     * @Route("/admin/user/delete/{id<\d+>}", name="admin_user_delete", methods={"POST"})
+     */
+    public function delete(Request $request, User $user = null, EntityManagerInterface $em): Response
+    {
+        // 404 ?
+        if (null === $user) {
+            throw $this->createNotFoundException('Utilisateur non trouvé.');
+        }
+
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $em->remove($user);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('admin_user_browse', [], Response::HTTP_SEE_OTHER);
+    }
+}
