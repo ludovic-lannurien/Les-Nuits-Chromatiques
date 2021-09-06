bb672e6a755449d7a916a97814e5df4b8eb9429f
Teddy DAHYOT <61130381+teddydah@users.noreply.github.com>
2021-09-06 11:49:11 +0200

    Update fixtures

--- src/DataFixtures/Data/ArtistData.php
+++ src/DataFixtures/Data/ArtistData.php
@@ -11,8 +11,43 @@ class ArtistData
             'description' => 'Bernard & Biatch (B&B) est un groupe à la frontière entre deux univers. D’un côté Olivier Bernard, saxophoniste de jazz professionnel, de l’autre côté Biatch, un duo de créatures mélomanes issu de la musique électronique. Deux plus un pour former un trio où Free Jazz et Deep House s’unissent. Une première représentation lors du festival « Interactions #2 » pose les bases de leur projet. Par la suite, ils sortent leur premier EP « Jeep Dazz », produit par les structures CHKT et Le Bloc. B&B dévoile toute sa personnalité en live. C’est un mélange impulsif et organique, entre boîtes à rythmes numériques, synthétiseurs analogiques et le feutre du saxophone. Un live où même Brutus et Néron ne peuvent que se laisser porter par leur univers chaleureux.',
             'picture' => 'https://chkt.fr/wp-content/uploads/2020/06/bernard-biatch-2-750x500.jpg',
             'videolink' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/HW11OF5PYCI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-                'event' => 'Vernissage',
+                'event' => 'Lumière sur cour',
                 'genre' => 'Musique'
+        ],
+
+        [
+            'firstname' => 'COACH RAOUL',
+            'lastname' => null,
+            'description' => 'Rappeur plus productif que ta mamie avec les tarte aux fruits.',
+            'picture' => 'https://scontent-cdg2-1.xx.fbcdn.net/v/t1.6435-9/227233603_3079777125639547_4597570207435771226_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=0debeb&_nc_ohc=NfqYIetVpEsAX_8OjLR&_nc_ht=scontent-cdg2-1.xx&oh=70a54a0d25c6de50200fd46623688d73&oe=6159860A',
+            'videolink' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/H6xptNB5SrY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+            'event' => '',
+            'genre' => 'Musique'
+        ],
+
+        [
+            'firstname' => 'KETBI',
+            'lastname' => null,
+            'description' => 'Jeune producteur dijonnais qui est fortement influencé par la house et les musiques brésiliennes.',
+            'picture' => 'https://scontent-cdg2-1.xx.fbcdn.net/v/t1.6435-9/224004468_3079792368971356_6870917417178969518_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=0debeb&_nc_ohc=b4iB0wjzwfoAX9J_3Hi&_nc_ht=scontent-cdg2-1.xx&oh=6bbe7d7045d9e3fc3f31c4be6a2c95a9&oe=615C6F6B',
+            'videolink' => '
+            Lien vidéo
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/Whf4IerCkJM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+            'event' => '',
+            'genre' => 'Musique'
+        ],
+
+        [
+            'firstname' => 'PUMPLBP',
+            'lastname' => null,
+            'description' => 'Producteur hip-hop, il n\'en ai pas à son premier coup d\'essai.',
+            'picture' => 'https://scontent-cdg2-1.xx.fbcdn.net/v/t1.6435-9/121626492_2853004508316811_1887832508183784187_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=0debeb&_nc_ohc=Jqu7Wylj09kAX-sIZUY&_nc_ht=scontent-cdg2-1.xx&oh=7b9498c9706c27110b43f7af5781682c&oe=615A5508',
+            'videolink' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/cARVKjTBn8c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+            'event' => [
+                'Route Nocturne',
+                'After-Work lumineux'
+            ],
+            'genre' => 'Musique'
         ],
 
         [
@@ -21,7 +56,7 @@ class ArtistData
             'description' => 'Amis d\'enfance, ce groupe est composé d\'un beat maker et de deux chanteurs.',
             'picture' => 'https://chkt.fr/wp-content/uploads/2020/10/DSCF5024-768x768.jpg',
             'videolink' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/dYx9iv0Hrlo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-                'event' => 'Apéro Lumière',
+                'event' => 'La forêt de lumière',
                 'genre' => 'Musique'
         ],
 
@@ -31,7 +66,37 @@ class ArtistData
             'description' => 'Jeune lyonnais extrêmement talentueux dans le mapping.',
             'picture' => 'https://chkt.fr/wp-content/uploads/2016/11/Interactions-2016-348.jpg',
             'videolink' => '<iframe title="vimeo-player" src="https://player.vimeo.com/video/215686233?h=5321db63dc" width="640" height="360" frameborder="0" allowfullscreen></iframe>',
-                'event' => 'Lumière sur cour',
+                'event' => 'Vernissage',
+                'genre' => 'Lumière'
+        ],
+
+        [
+            'firstname' => 'SAPIA',
+            'lastname' => null,
+            'description' => '',
+            'picture' => null,
+            'videolink' => '<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/335567162&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>',
+                'event' => 'Apéro Lumière',
+                'genre' => 'Lumière'
+        ],
+
+        [
+            'firstname' => 'SVINDRÖN',
+            'lastname' => null,
+            'description' => 'Svindrön, aka David Guerra, est un artiste sonore, compositeur, interprète, sound designer et programmeur, vivant en France. Multi-instrumentiste autodidacte et créateur de musique électronique, il se forme à la musique électroacoustique (Conservatoire de Chalon-sur-Saône, avec Jean-Marc WEBER) et aux techniques studio (CFPM Villeurbanne). C’est au travers d’un live électro IDM / techno qu’il fait ses armes sur la scène lyonnaise. Aujourd’hui, par ses pratiques et son parcours, il lie à sa production musicale la précision d’une approche électroacoustique, l’efficacité des rythmes techno, la puissance des nappes de la musique drone et la chaleur boisée d’une guitare, ou, d’une sanza. Sensible à l’art total, David Guerra crée et collabore sur de nombreux projets artistiques où l’art sonore, la danse, l’art visuel, l’art plastique... vivent dans un temps unique.',
+            'picture' => 'https://scontent-mrs2-1.xx.fbcdn.net/v/t1.18169-9/165323_474995521851_180255_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=cdbe9c&_nc_ohc=PX2ARIMt3p4AX-IRPZ8&_nc_ht=scontent-mrs2-1.xx&oh=76465b25b16186acc1fa64b3e46f02e7&oe=615A15C9',
+            'videolink' => '<iframe src="https://player.vimeo.com/video/161602302?h=8ec48a614b" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>',
+            'event' => 'Patrimoine illuminé',
+            'genre' => 'Musique'
+        ],
+
+        [
+            'firstname' => 'THEORIZ',
+            'lastname' => null,
+            'description' => 'Théoriz est un studio de création Art et Technologie spécialisé dans la conception installations immersives et de spectacles audiovisuels innovant.',
+            'picture' => 'https://scontent-mrs2-2.xx.fbcdn.net/v/t1.6435-9/96817794_1625343920967366_7211342655119163392_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=9267fe&_nc_ohc=vkF_PKjoDksAX9d7VHb&_nc_ht=scontent-mrs2-2.xx&oh=779d62ef435c501d00113090e0e8314c&oe=615D1C58',
+            'videolink' => '<iframe src="https://player.vimeo.com/video/194838088?h=ff41b03408&color=45B7D9" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>',
+                'event' => 'Route Nocturne',
                 'genre' => 'Lumière'
         ],
 
@@ -39,41 +104,10 @@ class ArtistData
             'firstname' => 'WECOMEINPEACE',
             'lastname' => null,
             'description' => 'WECOMEINPEACE a été fondé par Wilfried Della Rossa, Kimi Do et Thomas Mathieu Depuis 2008, ils produisent des évènements et des installations autour de concepts innovants et créatifs. Ils puisent leur inspiration dans différents domaines tels que le design, le graphisme, l’architecture et le jeu vidéo.',
-            'picture' => null,
+            'picture' => 'https://cdn.pixabay.com/photo/2010/12/13/10/34/video-2915_960_720.jpg',
             'videolink' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/1xJQ816CTKQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-                'event' => 'Patrimoine illuminé',
+                'event' => 'After-Work lumineux',
                 'genre' => 'Lumière'
-        ],
-
-        [
-            'firstname' => 'THEORIZ',
-            'lastname' => null,
-            'description' => 'Théoriz est un studio de création Art et Technologie spécialisé dans la conception installations immersives et de spectacles audiovisuels innovant.',
-            'picture' => null,
-            'videolink' => '<iframe src="https://player.vimeo.com/video/194838088?h=ff41b03408&color=45B7D9" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>',
-                'event' => 'La forêt de lumière',
-                'genre' => 'Lumière'
-        ],
-
-        [
-            'firstname' => 'SAPIA',
-            'lastname' => null,
-            'description' => 'test',
-            'picture' => null,
-            'videolink' => '<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/335567162&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>',
-                'event' => 'Vernissage',
-                'genre' => 'Lumière'
-        ],
-
-        [
-            'firstname' => 'David',
-            'lastname' => 'Guerra',
-            'description' => 'Svindrön, aka David Guerra, est un artiste sonore, compositeur, interprète, sound designer et programmeur, vivant en France.
-            Multi-instrumentiste autodidacte et créateur de musique électronique, il se forme à la musique électroacoustique (Conservatoire de Chalon-sur-Saône, avec Jean-Marc WEBER) et aux techniques studio (CFPM Villeurbanne). C’est au travers d’un live électro IDM / techno qu’il fait ses armes sur la scène lyonnaise. Aujourd’hui, par ses pratiques et son parcours, il lie à sa production musicale la précision d’une approche électroacoustique, l’efficacité des rythmes techno, la puissance des nappes de la musique drone et la chaleur boisée d’une guitare, ou, d’une sanza. Sensible à l’art total, David Guerra crée et collabore sur de nombreux projets artistiques où l’art sonore, la danse, l’art visuel, l’art plastique... vivent dans un temps unique.',
-            'picture' => null,
-            'videolink' => '<iframe src="https://player.vimeo.com/video/161602302?h=8ec48a614b" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>',
-                'event' => 'Apéro Lumière',
-                'genre' => 'Musique'
         ]
     ];
 }
--- src/DataFixtures/Data/PlaceData.php
+++ src/DataFixtures/Data/PlaceData.php
@@ -6,66 +6,75 @@ class PlaceData
 {
     public static $placeData = [
         [
-            'name' => 'Hôtel de Vogüé',
-            'address' => '8 Rue de la Chouette',
-            'latitude' => 47.33122550292202,
-            'longitude' => 5.043896453201671,
-            'description' => 'L\'Hôtel de Vogüé de Dijon en Côte-d\'Or est un hôtel particulier du xviie siècle. Il est situé 8 rue de la Chouette, près du chevet de l\'église Notre-Dame. L\'hôtel a été classé au titre des monuments historiques le 5 janvier 1911.',
-            'picture' => 'https://upload.wikimedia.org/wikipedia/commons/6/61/Vue_panoramique_de_Dijon_10.jpg'
-        ],
-
-        [
-            'name' => 'Musée des Beaux-Arts de Dijon',
-            'address' => '1 Rue Rameau',
-            'latitude' => 47.34670216857804,
-            'longitude' => 5.0303548436853385,
-            'description' => 'Le musée des Beaux-Arts de Dijon est l\'un des plus importants et l\'un des plus anciens musées de France. Situé au cœur de la ville, il occupe l\'ancien palais ducal, siège au XVᵉ siècle de l’État bourguignon.',
-            'picture' => 'https://upload.wikimedia.org/wikipedia/commons/e/e5/Entr%C3%A9e_du_mus%C3%A9e_des_Beaux-Arts.jpg'
-        ],
-
-        [
             'name' => 'Cour Bareuzai',
             'address' => '61 Rue des Godrans',
-            'latitude' => 47.32253972478744,
-            'longitude' => 5.038648910961855,
+            'latitude' => 47.323,
+            'longitude' => 5.039,
             'description' => 'Cour Bareuzai se situe à Dijon, Capitale de la région Bourgogne Franche Comté, au sein du centre-ville historique classé depuis juillet 2015 au patrimoine mondial de l’Unesco. Passage atypique, c’est à la fois un lieu unique et central permettant d’offrir une expérience nouvelle grâce à un projet de rénovation ambitieux.',
-            'picture' => 'https://chroniques-architecture.com/wp-content/uploads/2021/04/01-Cour-Bareuzai-Chapman-Taylor-@Stephane-Rouillard.jpg'
-        ],
-
-        [
-            'name' => 'La Vapeur',
-            'address' => '42 Avenue de Stalingrad',
-            'latitude' => 47.40590302174257,
-            'longitude' => 5.060213182853193,
-            'description' => 'Site officiel de La Vapeur, scène de musiques actuelles de la ville de Dijon. Concerts, expositions, projections, actions culturelles et artistiques.',
-            'picture' => 'https://www.souchier-boullet.com/wp-content/uploads/2019/01/lavapeur-dijon.jpg'
-        ],
-
-        [
-            'name' => 'Galeries Lafayette',
-            'address' => '41-49 Rue de la Liberté',
-            'latitude' => 47.43766312827879,
-            'longitude' => 5.014112175752159,
-            'description' => 'Galeries du Groupe Galeries Lafayette. Le bâtiment se situe à l\'angle de la rue du château et de la rue de la Liberté.',
-            'picture' => 'https://cdn-s-www.bienpublic.com/images/3B61ACB0-8B7E-4628-B529-E8F95B0CA2B8/NW_raw/1-quatre-vingt-dix-ans-apres-la-facade-des-galeries-lafayette-a-garde-toute-sa-prestance-en-particulier-en-plein-centre-ville-de-dijon-2-aujourd-hui-le-magasin-bourguignon-propose-plusieurs-centaines-de-marques-photos-p-b-1411740300.jpg'
+            'picture' => 'https://cdn.pixabay.com/photo/2021/07/10/18/37/carousel-6402074_960_720.jpg'
         ],
 
         [
             'name' => 'Cour de l\'ENSA',
             'address' => '3 Rue Michelet',
-            'latitude' => 47.45752729473983,
-            'longitude' => 5.014112017943309,
+            'latitude' => 47.458,
+            'longitude' => 5.014,
             'description' => 'La Cour de l\'ENSA est la cour de L\'École nationale supérieure d\'art de Dijon, anciennement l\'École nationale des beaux-arts de Dijon.',
-            'picture' => 'https://ensa-dijon.fr/wp-content/uploads/2020/10/cite-internationale-arts-paris.jpg'
+            'picture' => 'https://cdn.pixabay.com/photo/2015/07/31/11/45/library-869061_960_720.jpg'
+        ],
+
+        [
+            'name' => 'Galeries Lafayette',
+            'address' => '41-49 Rue de la Liberté',
+            'latitude' => 47.438,
+            'longitude' => 5.014,
+            'description' => 'Galeries du Groupe Galeries Lafayette. Le bâtiment se situe à l\'angle de la rue du château et de la rue de la Liberté.',
+            'picture' => 'https://cdn.pixabay.com/photo/2014/10/24/02/13/lafayette-galleries-500684_960_720.jpg'
         ],
 
         [
             'name' => 'Grand Hôtel La Cloche',
             'address' => '14 Place Darcy',
-            'latitude' => 47.32432869558158,
-            'longitude' => 5.034022511586989,
-            'description' => 'Grand Hôtel La Cloche de Dijon est un hôtel 5 étoiles classé monument historique qui propose des chambres et suites en plein centre ville.',
+            'latitude' => 47.324328695582,
+            'longitude' => 5.034022511587,
+            'description' => 'Le Grand Hôtel La Cloche de Dijon est un hôtel 5 étoiles classé monument historique qui propose des chambres et suites en plein centre ville.',
             'picture' => 'https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_1300,q_auto,w_2000/itemimages/48/69/48691_v4.jpeg'
+        ],
+
+        [
+            'name' => 'Hôtel de Vogüé',
+            'address' => '8 Rue de la Chouette',
+            'latitude' => 47.331,
+            'longitude' => 5.044,
+            'description' => 'L\'Hôtel de Vogüé de Dijon en Côte-d\'Or est un hôtel particulier du xviie siècle. Il est situé 8 rue de la Chouette, près du chevet de l\'église Notre-Dame. L\'hôtel a été classé au titre des monuments historiques le 5 janvier 1911.',
+            'picture' => 'https://cdn.pixabay.com/photo/2015/06/13/00/05/dijon-807632_960_720.jpg'
+        ],
+
+        [
+            'name' => 'La Vapeur',
+            'address' => '42 Avenue de Stalingrad',
+            'latitude' => 47.346,
+            'longitude' => 5.06,
+            'description' => 'Site officiel de La Vapeur, scène de musiques actuelles de la ville de Dijon. Concerts, expositions, projections, actions culturelles et artistiques',
+            'picture' => 'https://cdn.pixabay.com/photo/2018/05/10/11/34/concert-3387324_960_720.jpg'
+        ],
+        
+        [
+            'name' => 'Le SkatePark',
+            'address' => '12 Boulevard Trimolet',
+            'latitude' => 47.327,
+            'longitude' => 5.066,
+            'description' => 'Lieu incontournable de la scène skate dijonnaise.',
+            'picture' => 'https://cdn.pixabay.com/photo/2016/01/19/17/01/skateboarding-1149507_960_720.jpg'
+        ],
+
+        [
+            'name' => 'Musée des Beaux-Arts de Dijon',
+            'address' => '1 Rue Rameau',
+            'latitude' => 47.347,
+            'longitude' => 5.03,
+            'description' => 'Le musée des Beaux-Arts de Dijon est l\'un des plus importants et l\'un des plus anciens musées de France. Situé au cœur de la ville, il occupe l\'ancien palais ducal, siège au XVᵉ siècle de l’État bourguignon.',
+            'picture' => 'https://cdn.pixabay.com/photo/2017/12/23/19/54/dijon-3035882_960_720.jpg'
         ]
     ];
 }
