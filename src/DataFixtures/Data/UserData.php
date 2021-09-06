b3b41a5466217f0601700cd97c964bfafb167a2e
Teddy DAHYOT <61130381+teddydah@users.noreply.github.com>
2021-09-06 16:01:55 +0200

    Update UserData.php

--- src/DataFixtures/Data/UserData.php
+++ src/DataFixtures/Data/UserData.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures\Data;
 class UserData
 {
     public static $userData = [
+        ['email' => 'test@test.fr', 'roles' => 'ROLE_ADMIN', 'password' => 'test'],
         ['email' => 'admin@test.fr', 'roles' => 'ROLE_ADMIN', 'password' => 'admin'],
         ['email' => 'super-admin@test.fr', 'roles' => 'ROLE_SUPER_ADMIN', 'password' => 'super-admin']
     ];
